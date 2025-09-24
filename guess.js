37c6ef9433d0c893ff02fd9794b439352106cf93
moses <okonkwomoses158@gmail.com>
2025-09-24 18:18:49 +0100

    fix: corrected the js to perform the right functions

--- guess.js
+++ guess.js
@@ -5,41 +5,74 @@ const  enter_text = document.querySelector('.enter_text')
 
 
 let tries = 5;
+let computer_choice = Math.floor(Math.random() * 10) + 1
 
-
-
-const computer_choice = Math.floor(Math.random()*11)
 
 
 trial_count.value = tries;
-
+enter_text.textContent = "Enter a number from 1 to 10 to play"
 
 function check_values(){
-            const userGuess = parseInt(inputValue.value)
 
-            if(isNaN(userGuess)){
-                enter_text.textContent = "input a number to play"
-                return;
-            }
-            
-            if (userGuess === computer_choice){
-               enter_text.textContent = "congrations! you got the lucky number" ;
-                action_btn.disabled = true;
-                return
-            }else if(userGuess> computer_choice){
-                enter_text.textContent = "The number you chose is way bigger than the lucky number, try again"
-                tries--
-            }else{
-                enter_text.textContent = "The number you chose is smaller than the lucky number, try again.."
-                tries--
-            }
-            if (tries === 0 && userGuess !== computer_choice ){
-                enter_text.textContent = `Game over!! The lucky number was ${computer_choice}. \n Refresh to play agian`
-                action_btn.disabled = true;
-                
-            }
-            trial_count.value = tries;
-        
+    const userGuess = parseInt(inputValue.value)
+
+    if(isNaN(userGuess) || userGuess < 1 || userGuess > 10 ){
+        enter_text.textContent = "input a valid number to play"
+        return;
+
+    }
+    
+    if(userGuess === computer_choice){
+        enter_text.textContent = "congrations! you got the lucky number" ;
+        action_btn.textContent = "Play Again"
+        inputValue.disabled = true;
+    }else if(tries > 1){
+        tries--
+        trial_count.value = tries;
+        inputValue.disabled = false;
+        if( userGuess > computer_choice){
+            enter_text.textContent = `The number ${userGuess} is too high. Try again!!. You got ${tries} trials left.`
+        }else{
+            enter_text.textContent =   `The number ${userGuess} is too low. Try again!!. you got ${tries} trials left.`
+        }
+    }else{
+        tries = 0;
+        trial_count.value = tries;
+        enter_text.textContent = `Game over! The lucky number was ${computer_choice}. `;
+        action_btn.textContent =  "Play Again"  
+        inputValue.disabled = true;     
+    }
+    inputValue.value = "";
 }
- 
 
+function resetGame(){
+    tries = 5;
+    computer_choice = Math.floor(Math.random()*10) + 1;
+    trial_count.value = tries
+    enter_text.textContent = "Enter a number between 1 to 10 to play!."
+    action_btn.textContent = "Guess"
+    inputValue.value = "";
+    inputValue.disabled = false;
+    inputValue.focus()
+
+}
+
+action_btn.addEventListener('click', () => {
+    if(action_btn.textContent === "Play Again"){
+        resetGame();
+    }else{
+        check_values()
+    }
+})
+
+
+
+inputValue.addEventListener('keydown', (event) => {
+    if (event.key === "Enter") {
+        if (action_btn.textContent === "PlayAgain") {
+            resetGame();
+        } else {
+            check_values();
+        }
+    }
+});
